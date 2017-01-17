36b5ec35a969d4d98a21a5fb8754551e9cb03cfc
Matt Callahan <matt.callahan@concordusa.com>
2017-01-17 16:43:31 -0600

    jumbotron filtering by user

--- app/app.component.ts
+++ app/app.component.ts
@@ -17,7 +17,7 @@ import{Component }from '@angular/core';
     <div class = "row">
       <div class = "col-sm-4">
       <ul class = "list-group users-list"  *ngFor = "let user of users">
-        <li class = "list-group-item">
+        <li class = "list-group-item" (click) = "selectUser(user)">
         {{message}}, {{user.username}}.
         I know your real name is {{user.name}}
         </li>
@@ -28,6 +28,10 @@ import{Component }from '@angular/core';
       <div class = "col-sm-8">
         <div class = "jumbotron">
           <h1>My Angular2 App!</h1>
+            <div *ngIf = "activeUser">
+              <h4>Behold {{activeUser.username}}</h4>
+              <p>Their real name is {{activeUser.name}}</p>
+            </div>
         </div>
       </div>
     </div>
@@ -37,7 +41,10 @@ import{Component }from '@angular/core';
     <footer class = "text-center">
     Copyright &copy; 2016
     </footer>
-    `
+    `,
+    styles:[
+      
+    ]
 
 
 })
@@ -45,9 +52,13 @@ import{Component }from '@angular/core';
 export class AppComponent{
     message = "Hello";
     users = [
-      {id:12, name:'Chris', username:'XxsKILLosaurus_RexxX420'},
-      {id:12, name:'Pete', username:'__theGrImRePetEr'},
-      {id:12, name:'Matt', username:'_-_YOMAMA_-_'}
-  ]
+              {id:12, name:'Chris', username:'XxsKILLosaurus_RexxX420'},
+              {id:12, name:'Pete', username:'__theGrImRePetEr'},
+              {id:12, name:'Matt', username:'_-_YOMAMA_-_'}
+            ]
+    activeUser;
+    selectUser(userIn){
+      this.activeUser = userIn;
+    }
 
 }
--- dist/app/app.component.js
+++ dist/app/app.component.js
@@ -15,12 +15,16 @@ var AppComponent = (function () {
             { id: 12, name: 'Matt', username: '_-_YOMAMA_-_' }
         ];
     }
+    AppComponent.prototype.selectUser = function (userIn) {
+        this.activeUser = userIn;
+    };
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "\n    <header>\n\n      <nav class = \"navbar navbar-inverse\">\n\n        <div class = \"navbar-header\">\n          <a href = \"/\" class = \"navbar-brand\">My Angular2 App!</a>\n        </div>\n      </nav>\n    </header>\n\n    <main>\n    <div class = \"row\">\n      <div class = \"col-sm-4\">\n      <ul class = \"list-group users-list\"  *ngFor = \"let user of users\">\n        <li class = \"list-group-item\">\n        {{message}}, {{user.username}}.\n        I know your real name is {{user.name}}\n        </li>\n\n\n      </ul>\n      </div>\n      <div class = \"col-sm-8\">\n        <div class = \"jumbotron\">\n          <h1>My Angular2 App!</h1>\n        </div>\n      </div>\n    </div>\n\n\n    </main>\n    <footer class = \"text-center\">\n    Copyright &copy; 2016\n    </footer>\n    "
+        template: "\n    <header>\n\n      <nav class = \"navbar navbar-inverse\">\n\n        <div class = \"navbar-header\">\n          <a href = \"/\" class = \"navbar-brand\">My Angular2 App!</a>\n        </div>\n      </nav>\n    </header>\n\n    <main>\n    <div class = \"row\">\n      <div class = \"col-sm-4\">\n      <ul class = \"list-group users-list\"  *ngFor = \"let user of users\">\n        <li class = \"list-group-item\" (click) = \"selectUser(user)\">\n        {{message}}, {{user.username}}.\n        I know your real name is {{user.name}}\n        </li>\n\n\n      </ul>\n      </div>\n      <div class = \"col-sm-8\">\n        <div class = \"jumbotron\">\n          <h1>My Angular2 App!</h1>\n            <div *ngIf = \"activeUser\">\n              <h4>Behold {{activeUser.username}}</h4>\n              <p>Their real name is {{activeUser.name}}</p>\n            </div>\n        </div>\n      </div>\n    </div>\n\n\n    </main>\n    <footer class = \"text-center\">\n    Copyright &copy; 2016\n    </footer>\n    ",
+        styles: []
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
