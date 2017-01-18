44c8e566248881bfa64a2d333d7e03ae65de7371
Matt Callahan <mattcllhn@gmail.com>
2017-01-18 10:39:40 -0600

    added userFormComponent

--- app/users/userForm.component.ts
+++ app/users/userForm.component.ts
@@ -4,8 +4,24 @@ import {User} from '../shared/models/user';
 @Component({
   selector: 'user-form',
   template:`
-  I AM THE FORM
-
-  `
+    <form>
+      <div class = "form-group">
+        <input type = "text" class = "form-control" name = "name" required />
+      </div>
+      <div class = "form-group">
+        <input type = "text" class = "form-control" name = "userName" required />
+      </div>
+      <button type = "submit" class = "btn btn-lg btn-block btn-primary">
+        CREATE USER
+      </button>
+    </form>
+  `,
+  styles : [`
+    form {
+      padding : 5px;
+      background-color: #ecf0f1;
+      border-radius : 3px;
+    }
+    `],
 })
 export class UserFormComponent{}
--- dist/app/users/userForm.component.js
+++ dist/app/users/userForm.component.js
@@ -14,7 +14,8 @@ var UserFormComponent = (function () {
 UserFormComponent = __decorate([
     core_1.Component({
         selector: 'user-form',
-        template: "\n  I AM THE FORM\n\n  "
+        template: "\n    <form>\n      <div class = \"form-group\">\n        <input type = \"text\" class = \"form-control\" name = \"name\" required />\n      </div>\n      <div class = \"form-group\">\n        <input type = \"text\" class = \"form-control\" name = \"userName\" required />\n      </div>\n      <button type = \"submit\" class = \"btn btn-lg btn-block btn-primary\">\n        CREATE USER\n      </button>\n    </form>\n  ",
+        styles: ["\n    form {\n      padding : 5px;\n      background-color: #ecf0f1;\n      border-radius : 3px;\n    }\n    "],
     })
 ], UserFormComponent);
 exports.UserFormComponent = UserFormComponent;
