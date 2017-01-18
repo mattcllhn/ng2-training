a85104fd4efccb234c62c10950d808ee9ed49424
Matt Callahan <mattcllhn@gmail.com>
2017-01-18 11:14:44 -0600

    userForms hella validated

--- app/users/userForm.component.ts
+++ app/users/userForm.component.ts
@@ -4,14 +4,22 @@ import {User} from '../shared/models/user';
 @Component({
   selector: 'user-form',
   template:`
-    <form>
-      <div class = "form-group">
-        <input type = "text" class = "form-control" name = "name" required />
+    <form #formVar = "ngForm" (ngSubmit) = "createUser()" *ngIf = "active">
+    {{formVar.valid}}
+      <div class = "form-group" [ngClass] = "{'has-error': name.invalid && name.touched}">
+        <span class = "help-block" *ngIf = "name.invalid && name.touched">NAME IS REQUIRED</span>
+
+        <input type = "text" placeholder = "NAME" class = "form-control" name = "name" required
+        [(ngModel)] = "newUser.name" #name = "ngModel"
+        />
       </div>
-      <div class = "form-group">
-        <input type = "text" class = "form-control" name = "userName" required />
+      <div class = "form-group" [ngClass] = "{'has-error': userName.invalid && userName.touched}">
+        <span class = "help-block" *ngIf = "userName.invalid && userName.touched">USERNAME IS REQUIRED</span>
+        <input type = "text" placeholder = "USERNAME" class = "form-control" name = "userName" required
+        [(ngModel)] = "newUser.userName" #userName = "ngModel"
+        />
       </div>
-      <button type = "submit" class = "btn btn-lg btn-block btn-primary">
+      <button type = "submit"  class = "btn btn-lg btn-block btn-primary" [disabled] = "formVar.invalid">
         CREATE USER
       </button>
     </form>
@@ -24,4 +32,16 @@ import {User} from '../shared/models/user';
     }
     `],
 })
-export class UserFormComponent{}
+export class UserFormComponent{
+  newUser : User = new User();
+  active:boolean = true;
+
+  createUser(data){
+    console.log(this.newUser);
+
+    this.newUser = new User();
+    this.active = false;
+    setTimeout(()=> this.active = true,0);
+
+  }
+}
--- dist/app/users/userForm.component.js
+++ dist/app/users/userForm.component.js
@@ -6,15 +6,25 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 var core_1 = require("@angular/core");
+var user_1 = require("../shared/models/user");
 var UserFormComponent = (function () {
     function UserFormComponent() {
+        this.newUser = new user_1.User();
+        this.active = true;
     }
+    UserFormComponent.prototype.createUser = function (data) {
+        var _this = this;
+        console.log(this.newUser);
+        this.newUser = new user_1.User();
+        this.active = false;
+        setTimeout(function () { return _this.active = true; }, 0);
+    };
     return UserFormComponent;
 }());
 UserFormComponent = __decorate([
     core_1.Component({
         selector: 'user-form',
-        template: "\n    <form>\n      <div class = \"form-group\">\n        <input type = \"text\" class = \"form-control\" name = \"name\" required />\n      </div>\n      <div class = \"form-group\">\n        <input type = \"text\" class = \"form-control\" name = \"userName\" required />\n      </div>\n      <button type = \"submit\" class = \"btn btn-lg btn-block btn-primary\">\n        CREATE USER\n      </button>\n    </form>\n  ",
+        template: "\n    <form #formVar = \"ngForm\" (ngSubmit) = \"createUser()\" *ngIf = \"active\">\n    {{formVar.valid}}\n      <div class = \"form-group\" [ngClass] = \"{'has-error': name.invalid && name.touched}\">\n        <span class = \"help-block\" *ngIf = \"name.invalid && name.touched\">NAME IS REQUIRED</span>\n\n        <input type = \"text\" placeholder = \"NAME\" class = \"form-control\" name = \"name\" required\n        [(ngModel)] = \"newUser.name\" #name = \"ngModel\"\n        />\n      </div>\n      <div class = \"form-group\" [ngClass] = \"{'has-error': userName.invalid && userName.touched}\">\n        <span class = \"help-block\" *ngIf = \"userName.invalid && userName.touched\">USERNAME IS REQUIRED</span>\n        <input type = \"text\" placeholder = \"USERNAME\" class = \"form-control\" name = \"userName\" required\n        [(ngModel)] = \"newUser.userName\" #userName = \"ngModel\"\n        />\n      </div>\n      <button type = \"submit\"  class = \"btn btn-lg btn-block btn-primary\" [disabled] = \"formVar.invalid\">\n        CREATE USER\n      </button>\n    </form>\n  ",
         styles: ["\n    form {\n      padding : 5px;\n      background-color: #ecf0f1;\n      border-radius : 3px;\n    }\n    "],
     })
 ], UserFormComponent);
